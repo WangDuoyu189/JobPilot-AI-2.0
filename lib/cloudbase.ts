e6bff1ec68b6527b3ce37e4e0045b51bb75504af
WangDuoyu189 <2682704450@qq.com>
2026-09-20 15:30:08 +0800

    feat(auth): add CloudBase Auth client

---
+++ lib/cloudbase.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import cloudbase from "@cloudbase/js-sdk";
+
+const envId =
+  process.env.NEXT_PUBLIC_CLOUDBASE_ENV_ID ||
+  "jobpilot-prod-d5gawdy5ife107d47";
+
+export const app = cloudbase.init({
+  env: envId,
+});
+
+export const auth = app.auth({
+  persistence: "local",
+});
